0fb1286c10024a53526f4ddc1b455f402c049067
moiz2405 <km856307@gmail.com>
2025-01-02 17:36:48 +0000

    Modified page.tsx in chatbot

--- app/chatbot/page.tsx
+++ app/chatbot/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 // import LanguageModelComponent from '@/components/chatbox/LanguageModel'
 import ChatInterface from '@/components/chatbox/ChatInterface'
+
 const page = () => {
   return (
-    <>
-    <ChatInterface/>
-    </>
+    <div
+      className=" p-5 overflow-hidden bg-gray-900"
+      style={{ height: 'calc(100vh - 60px)', overflow: 'hidden' }}
+    >
+      <ChatInterface />
+    </div>
   )
 }
 
