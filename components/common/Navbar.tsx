267336fd28bad23db5ae9bb8b5fa58fd23aa6bf1
moiz2405 <km856307@gmail.com>
2025-01-02 17:36:48 +0000

    Modified Navbar.tsx

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -21,22 +21,21 @@ const Navbar = React.forwardRef<HTMLElement, { children?: React.ReactNode }>((pr
   return (
     <nav
       ref={ref}
-      className={`fixed w-full top-0 left-0 z-50 transition-all duration-300 bg-gray-800/70 backdrop-blur-md ${
-        isScrolled ? "shadow-md" : ""
-      }`}
+      className={`fixed w-full top-0 left-0 z-50 transition-all duration-300 bg-gray-800/70 backdrop-blur-md ${isScrolled ? "shadow-md" : ""
+        }`}
       aria-label="Main navigation"
     >
       <div className="container mx-auto px-4 py-2">
         <div className="flex h-16 items-center justify-between">
           {/* Logo and Title */}
           <Link href="/" className="flex items-center space-x-2" aria-label="Homepage">
-            <Image
+            {/* <Image
               src="/images/home/logo-1.png"
               alt="Fluent.ai Logo"
               width={32}
               height={32}
               className="rounded-lg"
-            />
+            /> */}
             <span className="text-lg font-bold text-white">Fluent.ai</span>
           </Link>
 
